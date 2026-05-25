44bacd0049db1ed5606729116d946572f339b5b8
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-05-25 05:10:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import decimal
+
+# precision
+decimal.getcontext().prec = 100
+
+def PI(iterations):
+    digit = 1
+    for i in range(3,(iterations)*2,4):
+        digit -= decimal.Decimal(1) / decimal.Decimal(i)
+        digit += decimal.Decimal(1) / decimal.Decimal((i + 2))
+
+    return 4 * digit 
+
+if __name__ == "__main__":
+    iterations = 10000000
+    digit = PI(iterations)
+    print(digit)
